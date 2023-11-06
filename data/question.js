30b3633532bfe81f6993a20366a9d389ad0c630a
Milan Ismagulov <4k.milan15@gmail.com>
2023-11-06 12:56:32 +0500

    first commit

---
+++ data/question.js
@@ -0,0 +1,10 @@
+let questions = [
+    {
+        text: '1',
+        right: 0,
+        answers: ['1', '2', '3', '4'],
+    },
+];
+
+const numberOfQuestion = 1; 
+const numberOfQuestionSum = questions.length+1;
---
+++ scripts/main.js
@@ -0,0 +1,21 @@
+// Находим title
+const titlePlc = document.querySelector('#title_text_place')
+
+// Находим Black Header
+const blackHeaderPlc = document.querySelector('#black_header_place')
+
+// Находим Black Header
+const popupStepsWindow = document.querySelector('#popup_text_place')
+
+titlePlc.innerHTML = themeOfDEC
+blackHeaderPlc.innerHTML = themeOfDEC
+
+
+popUpTextArr = textOfStepsInPopUp.split('\n')
+
+popUpTextArr.forEach(function(array, index, item){
+    let popUpHeadElem = document.createElement('h3')
+    popUpHeadElem.classList.add('popup_list')
+    popupStepsWindow.appendChild(popUpHeadElem)
+    popUpHeadElem.innerHTML = `<span>${index+1}. </span> ${array}`
+});
